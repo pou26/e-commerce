903e7e7abace590a13a8eb663787038d5bfe95b6
pou26 <poushaliaich1999@gmail.com>
2023-01-14 21:04:23 +0530

    auth

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import BrandLogo from 'images/amazon-logo2.png';
 import { Link } from 'react-router-dom';
 import './login.css'
+import {useRef,useState,useEffect} from 'react'
+// import { setUISelectionRaw } from '@testing-library/user-event/dist/types/document/UI';
 // import React from 'react';
 
 // import BrandLogo from 'images/amazon-logo.png';
@@ -21,7 +23,52 @@ import './login.css'
 // export default Logo;
 
 function LoginPage() {
+  const userRef= useRef();
+  const errRef= useRef();
+
+  // const[user,setUser]=useState('');
+  const [email,setEmail]= useState('');
+  const [password,setPassword]= useState('');
+  const [errMsg,setErrMsg]=useState('');
+  const[success,setSuccess]=useState(false);
+
+  useEffect(()=>{
+    userRef.current.focus();
+  },[])
+
+  useEffect(()=>{
+    setErrMsg('');
+  },[email,password])
+
+  const handleSubmit=async(e)=>{
+    e.preventDefault();
+    console.log(email,password);
+    setEmail('');
+    setPassword('');
+    setSuccess(true);
+  }
+
+  const signIn=e=>{
+    e.preventDefault();
+  }
+  const register= e=>{
+    e.preventDefault();
+  }
   return (
+    <>
+    {success ? (
+      <section>
+        <h1>Successfully logged in!</h1>
+        <br />
+        <p>
+          {/* <a href="#">Home</a> */}
+        </p>
+      </section>
+    ) :(
+    <section>
+    <p ref={errRef} className={errMsg ? "errmsg":"offscreen"}aria-live="assertive">
+    {errMsg}
+    </p>
     <div className='login'>
       <Link to="/" className="logo">
         
@@ -31,12 +78,14 @@ function LoginPage() {
       </Link>
       <div className='login_container'>
         <h1>Sign-in</h1>
-        <form>
-          <h5>E-mail or phone number</h5>
-          <input type='text' />
-          <h5>Password</h5>
-          <input type='password' />
-          <button className="login_button">Continue</button>
+        <form onSubmit={handleSubmit}>
+          {/* <h5>E-mail or phone number</h5> */}
+          <label htmlFor="email">E-mail or phone number</label>
+          <input type='text' id="email" ref={userRef} value={email} onChange={e=>setEmail(e.target.value)} required />
+          {/* <h5>Password</h5> */}
+          <label htmlFor="password">Password</label>
+          <input type='password' id="password" value={password} onChange={e=>setPassword(e.target.value)}/>
+          <button type='submit' onClick={signIn}className="login_button">Continue</button>
         </form>
         <p className="privacy">
         By continuing, you agree to Amazon's Conditions of Use and Privacy Notice
@@ -44,8 +93,13 @@ function LoginPage() {
         <ul>
           <li>Need help?</li>
         </ul>
-        <p className="text">New to Amazon?</p>
-        <button className="signup_button">Create your Amazon account</button>
+        <p className="text">New to Amazon?
+        {/* <br />
+        <span className="line">
+          <a href="#"></a>
+        </span> */}
+        </p>
+        <button onClick={register} className="signup_button" a href="#">Create your Amazon account</button>
         <table>
           <tr>
             <td>Conditions of Use</td>
@@ -59,6 +113,9 @@ function LoginPage() {
       </div>
 
   </div>
+  </section>
+     )}
+     </>
   )
 }
 
